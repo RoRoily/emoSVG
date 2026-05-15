d0ba8a1b3044eb6a5da47ae2f222806a47d003d3
RoRoily <zhengmohum@gmail.com>
2026-05-15 15:25:57 +0800

    modify __init.py

--- src/modules/meme_animator/__init__.py
+++ src/modules/meme_animator/__init__.py
@@ -1,5 +1,19 @@
-from .animator import MemeAnimator
 from .motion_designer import MotionDesigner
 from .schemas import AnimationRequest, AnimationResult, MemeExpression, SquashParams
 
-__all__ = ["MemeAnimator", "MotionDesigner", "AnimationRequest", "AnimationResult", "MemeExpression", "SquashParams"]
+__all__ = [
+    "MemeAnimator",
+    "MotionDesigner",
+    "AnimationRequest",
+    "AnimationResult",
+    "MemeExpression",
+    "SquashParams",
+]
+
+
+def __getattr__(name: str):
+    if name == "MemeAnimator":
+        from .animator import MemeAnimator
+
+        return MemeAnimator
+    raise AttributeError(name)
